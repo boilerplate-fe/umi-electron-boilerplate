8c5b463f42dd552f2075c066c53717ca357c7e9e
DiamondYuan <admin@diamondyuan.com>
2020-03-31 16:41:36 +0800

    :sparkles: support phash

--- src/services/testService.ts
+++ src/services/testService.ts
@@ -1,9 +1,15 @@
-import { Get, JsonController } from 'routing-controllers';
+import { Get, JsonController, QueryParams } from 'routing-controllers';
+import { ImagePHash } from 'root/base/node/imagePHash';
 
 @JsonController('/TestService')
 export class TestService {
   @Get('/hello')
   async hello() {
     return 'Hello World';
+  }
+
+  @Get('/imagePHash')
+  async imagePHash(@QueryParams({ parse: true }) query: { path: string }) {
+    return ImagePHash.getImagePHash(query.path);
   }
 }
