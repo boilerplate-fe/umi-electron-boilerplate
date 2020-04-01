a078297a0500ccb9bfdfb1798dd5e9a9b158b295
DiamondYuan <admin@diamondyuan.com>
2020-04-01 16:09:47 +0800

    fix: test compressing

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,12 +1,5 @@
-import 'reflect-metadata';
 import { app, BrowserWindow } from 'electron';
-import { start } from './server';
-import { dev } from 'root/base/common/platform';
-const kill = require('kill-port');
-
-export function load(vanalandExtension: string) {
-  (global as any).__non_webpack_require__(vanalandExtension);
-}
+import { zip } from 'compressing';
 
 let mainWindow: Electron.BrowserWindow | null;
 
@@ -20,19 +13,13 @@ async function createWindow(): Promise<void> {
       devTools: true,
     },
   });
-  await mainWindow.loadURL('http://localhost:8888');
   mainWindow.on('closed', () => {
     mainWindow = null;
   });
 }
 
 app.on('ready', async () => {
-  if (dev) {
-    await kill(8887);
-  } else {
-    await kill(8888);
-  }
-  await start(!dev);
+  console.log(zip);
   await createWindow();
 });
 
