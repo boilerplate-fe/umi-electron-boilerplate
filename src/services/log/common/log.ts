b60cf9ce6b63c9df851cb08858b915b857363346
DiamondYuan <admin@diamondyuan.com>
2020-04-29 22:17:39 +0800

    :sparkles: add logger

---
+++ src/services/log/common/log.ts
@@ -0,0 +1,12 @@
+import { Level } from 'log4js';
+
+export interface ILogger {
+  getLevel(): Level;
+  setLevel(level: Level): void;
+  trace(message: any, ...args: any[]): void;
+  debug(message: any, ...args: any[]): void;
+  info(message: any, ...args: any[]): void;
+  warn(message: any, ...args: any[]): void;
+  error(message: any, ...args: any[]): void;
+  fatal(message: any, ...args: any[]): void;
+}
