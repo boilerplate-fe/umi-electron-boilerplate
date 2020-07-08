4dc26210e57666a7e43534617111b48e9c3ba624
DiamondYuan <admin@diamondyuan.com>
2020-07-08 08:55:04 +0800

    :recycle: use eumi

--- build/webpack.main.config.js
+++ build/webpack.main.config.js
@@ -1,28 +1,17 @@
 const path = require('path');
 
 function srcPaths(src) {
-  return path.join(__dirname, src);
+  return path.join(__dirname, '..', src);
 }
 
-const isEnvProduction = process.env.NODE_ENV === 'production';
 const isEnvDevelopment = process.env.NODE_ENV === 'development';
 
 // #region Common settings
 const commonConfig = {
-  entry: './src/main/main.ts',
+  entry: srcPaths('src/main/main.ts'),
   target: 'electron-main',
-  externals: [
-    {
-      log4js: `require('log4js')`,
-      sqlite3: `require('sqlite3')`,
-      express: `require('express')`,
-      encoding: `require('encoding')`,
-      'routing-controllers': `require('routing-controllers')`,
-      'any-promise': 'Promise',
-    },
-  ],
   devtool: isEnvDevelopment ? 'source-map' : false,
-  mode: isEnvProduction ? 'production' : 'development',
+  mode: isEnvDevelopment ? 'development' : 'production',
   output: { path: srcPaths('dist'), filename: 'main.bundle.js' },
   node: { __dirname: false, __filename: false },
   resolve: {
--- readme.md
+++ readme.md
@@ -1,10 +1,55 @@
 # umi-electron-boilerplate
 
-- umi
-- typescript
-- antd
-- web and electron
+> Powered by [eumi](https://github.com/diamondYuan/eumi).
 
-## 公告
+- UmiJS (render)
+- Webpack (main)
+- Typescript
+- Ant Design
+- ESLint
 
-正在开发中，欢迎关注 [eumi](https://github.com/diamondYuan/eumi).
+## 如何使用
+
+### 安装依赖
+
+```
+$ yarn
+```
+
+### 开发
+
+```
+$ yarn dev
+```
+
+浏览器打开 `http://localhost:8888` 即可预览。
+
+可以尝试修改 `src/renderer/pages/index.tsx` 和 `src/main/main.ts` 的内容，页面会自动刷新。
+
+### 编译
+
+```
+$ yarn build
+$ yarn start # 启动编译后的程序
+```
+
+### 打包
+
+```
+$ yarn release:win
+$ yarn release:mac
+```
+
+## 自定义配置
+
+### 主进程配置
+
+配置文件为 `build/webpack.main.config.js`,可以参考 [webpack](https://webpack.js.org/concepts/)。
+
+### 渲染进程配置
+
+配置文件为 `src/renderer/config/config.ts`,可以参考 [umi](https://umijs.org/config)。
+
+### 打包配置
+
+配置文件为 `electron-builder.json`,可以参考 [electron-builder](https://www.electron.build/configuration/configurations)。
